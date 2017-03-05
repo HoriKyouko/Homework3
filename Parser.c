f07c44833f7a20c020745ed23cf410a9babf81e3
Rennar <jwilliamson@knights.ucf.edu>
2017-03-05 16:43:48 -0500

    Parser update

--- Parser.c
+++ Parser.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 
-#define MAX_SYMBOL_TABLE_SIZE 500
+#define MAX_token_TABLE_SIZE 500
+#define MAX_CODE_LENGTH 500
 
 typedef enum {
 nulsym = 1, identsym, numbersym, plussym, minussym,
@@ -12,6 +13,12 @@ whilesym, dosym, callsym, constsym, varsym, procsym, writesym,
 readsym , elsesym
 } token_type;
 
+typedef enum {
+    op_lit = 1, op_rtn, op_lod, op_sto, op_cal, op_inc, op_jmp, op_jpc,
+    op_siop, op_sior, op_sioh, op_neg, op_add, op_sub, op_mul, op_div,
+    op_odd, op_mod, op_eql, op_lss, op_leq, op_gtr, op_geq
+} op_type
+
 typedef struct  {
 	int kind; 		// symbol = 0, const = 1, var = 2, proc = 3, keyword = 4;
 	int type;       // token_type
@@ -19,37 +26,49 @@ typedef struct  {
 	int val; 		// number (ASCII value)
 	int level; 		// L level
 	int addr; 		// M address
-} symbol;
+} token;
 
-void Program(symbol* tokens, int * currentElement);
-void Block(symbol* tokens, int * currentElement, int level);
-void ConstDecl(symbol*tokens, int * currentElement);
-void VarDecl(symbol*tokens, int*currentElement);
-void ProcDecl(symbol*tokens, int*currentElement, int level);
-void Statement(symbol*tokens, int*currentElement);
-void Condition(symbol * tokens, int * currentElement);
-void Expression(symbol*tokens, int *currentElement);
-void Term(symbol * tokens, int *currentElement);
-void Factor(symbol*tokens, int*currentElement);
+typedef struct {
+int op;		// op code
+int r;      // register
+int  l;   	// level
+int  m;   	// moffset
+} instruction;
+
+void Program(token* tokens, int * currentElement);
+void Block(token* tokens, int * currentElement, int level);
+void ConstDecl(token*tokens, int * currentElement);
+void VarDecl(token*tokens, int*currentElement);
+void ProcDecl(token*tokens, int*currentElement, int level);
+void Statement(token*tokens, int*currentElement);
+void Condition(token * tokens, int * currentElement);
+void Expression(token*tokens, int *currentElement);
+void Term(token * tokens, int *currentElement);
+void Factor(token*tokens, int*currentElement);
 
 int isRelation(int type);
 
 int main(){
-    symbol tokens[MAX_SYMBOL_TABLE_SIZE]
+    int currentElement = 0;
+    int cx = 0;
 
+    instruction code[];
+    token tokens[MAX_token_TABLE_SIZE]
+
+    Program(tokens, &currentElement)
 
 
 
 }
 
-void Program(symbol* tokens, int * currentElement){
+void Program(token* tokens, int * currentElement){
     Block(tokens, currentElement, 0);
     if(tokens[*currentElement].type != periodsym){
         //ERROR
     }
 }
 
-void Block(symbol* tokens, int * currentElement, int level){
+void Block(token* tokens, int * currentElement, int level){
     if(tokens[*currentElement].type == constsym){
         ConstDecl(tokens, currentElement);
     if(tokens[*currentElement.type == varsym){
@@ -61,7 +80,7 @@ void Block(symbol* tokens, int * currentElement, int level){
     Statement(tokens,currentElement);
 }
 
-void ConstDecl(symbol*tokens, int * currentElement){
+void ConstDecl(token*tokens, int * currentElement){
 
     do{
         *currentElement = *currentElement + 1;
@@ -86,7 +105,7 @@ void ConstDecl(symbol*tokens, int * currentElement){
 
 }
 
-void VarDecl(symbol*tokens, int*currentElement){
+void VarDecl(token*tokens, int*currentElement){
     do{
         *currentElement = *currentElement + 1;
         if(tokens[*currentElement].type!=identsym){
@@ -101,15 +120,15 @@ void VarDecl(symbol*tokens, int*currentElement){
     *currentElement = *currentElement+1;
 }
 
-void ProcDecl(symbol*tokens, int*currentElement, int level){
+void ProcDecl(token*tokens, int*currentElement, int level){
     while(tokens[*currentElement].type == procsym){
         *currentElement = *currentElement+1;
-        if(tokens[currentElement].type!= identsym){
+        if(tokens[*currentElement].type!= identsym){
             // ERROR
         }
         // ENTER(procedure, ident);
         *currentElement = *currentElement+1;
-        if(symbol[*currentElement].type != semicolonsym){
+        if(tokens[*currentElement].type != semicolonsym){
             // ERROR
         }
         *currentElement = *currentElement+1;
@@ -121,14 +140,14 @@ void ProcDecl(symbol*tokens, int*currentElement, int level){
     }
 }
 
-void Statement(symbol*tokens, int*currentElement){
+void Statement(token*tokens, int*currentElement){
     if(tokens[*currentElement].type == identsym){
         *currentElement = *currentElement+1;
         if(tokens[*currentElement].type!= becomessym){
             // Error
         }
         *currentElement = *currentElement+1;
-        Expression(symbol,currentElement);
+        Expression(tokens,currentElement);
     } else if(tokens[*currentElement].type==callsym){
         *currentElement = *currentElement+1;
         if(tokens[*currentElement].type!=identsym){
@@ -162,7 +181,7 @@ void Statement(symbol*tokens, int*currentElement){
     }
 }
 
-void Condition(symbol * tokens, int * currentElement){
+void Condition(token * tokens, int * currentElement){
     if(tokens[*currentElement].type == oddsym){
         *currentElement = *currentElement+1;
         Expression(tokens,currentElement);
@@ -176,27 +195,32 @@ void Condition(symbol * tokens, int * currentElement){
     }
 }
 
-void Expression(symbol*tokens, int *currentElement){
-    if(symbol[*currentElement].type == plussym){
+void Expression(token*tokens, int *currentElement){
+    int addOp = tokens[*currentElement].type;
+    if(tokens[*currentElement].type == plussym || tokens[*currentElement].type == minussym){
         *currentElement = *currentElement+1;
+        if(addOp == minussym){
+            // need to figure out r, l , and m
+            emit(op_neg, 0, 0, 0);
+        }
     }
     Term(tokens, currentElement);
-    while(tokens[*currentElement].type == plussym){
+    while(tokens[*currentElement].type == plussym || tokens[*currentElement].type == minussym){
         *currentElement = *currentElement+1;
         Term(tokens, currentElement)
     }
 }
 
-void Term(symbol * tokens, int *currentElement){
+void Term(token * tokens, int *currentElement){
     Factor(tokens, currentElement);
     *currentElement = *currentElement+1;
-    while(tokens[*currentElement].type == multsym){
+    while(tokens[*currentElement].type == multsym || tokens[*currentElement].type == slashsym){
         *currentElement = *currentElement+1;
         Factor(tokens, currentElement)
     }
 }
 
-void Factor(symbol*tokens, int*currentElement){
+void Factor(token*tokens, int*currentElement){
     if(tokens[*currentElement].type == identsym){
         *currentElement = *currentElement+1;
     } else if(tokens[*currentElement].type == numbersym){
@@ -211,6 +235,19 @@ void Factor(symbol*tokens, int*currentElement){
     } else{
         // ERROR
     }
+}
+
+void emit(int op, int r, int l, int m, int * cx){
+    if(*cx > MAX_CODE_LENGTH){
+        // ERROR
+    } else{
+        code[*cx].op = op;
+        code[*cx].r = r;
+        code[*cx].l = l;
+        code[*cx].m = m;
+        *cx = *cx+1;
+    }
+
 }
 
 int isRelation(int type){
